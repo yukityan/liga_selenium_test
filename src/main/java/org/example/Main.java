e80c7d1ce150fc1f29b132000fba209c43133d71
Yki <bykanova.juliya18@gmail.com>
2025-04-18 08:51:25 -0700

    second commit/2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.printf("second commit");
+        System.out.print("Hello and welcome!");
+        System.out.print("second commit");
+        System.out.print("first commit");
         }
     }
